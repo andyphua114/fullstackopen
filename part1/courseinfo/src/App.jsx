1498f0fc685a1c8b5c94a0d55e11ec5febb09494
wphua3 <wphua3@gatech.edu>
2023-12-13 21:12:28 +0800

    Initial commit for Ex 1.1-1.2

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,49 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.part[0]} {props.exercise[0]}</p>
+      <p>{props.part[1]} {props.exercise[1]}</p>
+      <p>{props.part[2]} {props.exercise[2]}</p>
+    </>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part} exercise={props.exercise} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.first + props.second + props.third}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={[part1, part2, part3]} exercise={[exercises1, exercises2, exercises3]} />
+      <Total first={exercises1} second={exercises2} third={exercises3} />
+    </div>
+  )
+}
+
+export default App
