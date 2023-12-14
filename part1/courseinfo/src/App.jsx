ac4c7618283fd9038add4d75bc3dd445ae9a6f42
wphua3 <wphua3@gatech.edu>
2023-12-14 16:45:34 +0800

    Complete until Ex 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -6,42 +6,50 @@ const Header = (props) => {
 
 const Part = (props) => {
   return (
-    <>
-      <p>{props.part[0]} {props.exercise[0]}</p>
-      <p>{props.part[1]} {props.exercise[1]}</p>
-      <p>{props.part[2]} {props.exercise[2]}</p>
-    </>
+    <p>{props.part.name} {props.part.exercises}</p>
   )
 }
 
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part} exercise={props.exercise} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   )
 }
 
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.first + props.second + props.third}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content part={[part1, part2, part3]} exercise={[exercises1, exercises2, exercises3]} />
-      <Total first={exercises1} second={exercises2} third={exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
