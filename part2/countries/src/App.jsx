e05c8d7a7dcf96f335a3e6d7d6dc11491975a4bf
wphua3 <wphua3@gatech.edu>
2023-12-20 19:03:32 +0800

    Complete until Ex 2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -7,7 +7,6 @@ const CountryForm = (props) => {
     <div>
       find countries &nbsp;
       <input value={props.userCountry} onChange={props.handleCountry} /> &nbsp;
-      <button onClick={props.toggleReset}>Reset</button>
     </div>
   )
 }
@@ -34,41 +33,55 @@ const DisplayFlag = ({ flag }) => {
   )
 }
 
+const Weather = (props) => {
+  const [weather, setWeather] = useState(null)
+
+  const filteredCountries = props.filteredCountries
+
+  countryService
+    .getWeather(filteredCountries[0].latlng[0], filteredCountries[0].latlng[1])
+    .then(weather => setWeather(weather))
+
+  if (weather) {
+    return (
+      <>
+        {/* <p>temperature {weather.current.temp_c} Celcius</p>
+      <img src={weather.current.condition.icon}></img>
+      <p>wind {(weather.current.wind_mph/2.237).toFixed(2)} m/s</p> */}
+        <p>temperature {weather.main.temp} Celcius</p>
+        <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}></img>
+        <p>wind {weather.wind.speed} m/s</p>
+      </>
+    )
+  }
+}
+
+
 const Display = (props) => {
-  const allCountries = props.allCountries
-  const filteredCountries = allCountries.filter(country => country.name.common.toLowerCase().includes(props.userCountry.toLowerCase()))
+  // const allCountries = props.allCountries
+  // const filteredCountries = allCountries.filter(country => country.name.common.toLowerCase().includes(props.userCountry.toLowerCase()))
+  const filteredCountries = props.filteredCountries
+  const filteredCountriesLength = props.filteredCountries.length
 
   if (props.userCountry === "") {
-    useEffect(() => {
-      props.setOneCountry(null);
-    })
     return (
       <DisplayMessage message="Enter country name" />
     )
   }
-  else if (filteredCountries.length === 0) {
-    useEffect(() => {
-      props.setOneCountry(null);
-    })
+  else if (filteredCountriesLength === 0) {
     return (
       <DisplayMessage message="No matches. Please try again" />
     )
   }
-  else if (filteredCountries.length > 10) {
-    useEffect(() => {
-      props.setOneCountry(null);
-    })
+  else if (filteredCountriesLength > 10) {
     return (
       <DisplayMessage message="Too many matches, specify another filter" />
     )
   }
-  else if (filteredCountries.length === 1) {
-    useEffect(() => {
-      props.setOneCountry(null);
-    })
+  else if (filteredCountriesLength === 1) {
     return (
       <div>
-        <h2>{filteredCountries[0].name.common}</h2>
+        <h1>{filteredCountries[0].name.common}</h1>
         <p>capital {filteredCountries[0].capital[0]}
           <br />
           area {filteredCountries[0].area}
@@ -80,44 +93,47 @@ const Display = (props) => {
           )}
         </ul>
         <DisplayFlag flag={filteredCountries[0].flags.png} />
+        <Weather filteredCountries={filteredCountries} />
       </div>
     )
   }
-  else if (props.oneCountry) {
+  else if ((filteredCountriesLength <= 10) && (!props.show)) {
     return (
       <div>
-        <h2>{props.oneCountry.name.common}</h2>
-        <p>capital {props.oneCountry.capital[0]}
-          <br />
-          area {props.oneCountry.area}
-        </p>
-        <h3>languages:</h3>
-        <ul>
-          {Object.values(props.oneCountry.languages).map(language =>
-            <DisplayLanguage key={language} language={language} />
-          )}
-        </ul>
-        <DisplayFlag flag={props.oneCountry.flags.png} />
-      </div>
+        {filteredCountries.map(country => {
+          return (
+            <div key={country.name.common}>
+              {country.name.common} &nbsp;
+              <button onClick={() => props.toggleShow(country.name.common)}>show</button>
+            </div>
+          )
+        })
+        }
+      </div >
     )
   }
-  useEffect(() => {
-    props.setOneCountry(null);
-  })
-  return (
-    <div>
-      {filteredCountries.map(country => {
-        return (
-          <div key={country.name.common}>
-            {country.name.common} &nbsp;
-            <button onClick={() => props.toggleShow(country.name.common, allCountries)}>show</button>
-          </div>
-        )
-      })
-
-      }
-    </div >
-  )
+  // else if (props.show) {
+  //   return (
+  //     <div>
+  //       <h1>{props.oneCountry.name.common}</h1>
+  //       <p>capital {props.oneCountry.capital[0]}
+  //         <br />
+  //         area {props.oneCountry.area}
+  //       </p>
+  //       <h3>languages:</h3>
+  //       <ul>
+  //         {Object.values(props.oneCountry.languages).map(language =>
+  //           <DisplayLanguage key={language} language={language} />
+  //         )}
+  //       </ul>
+  //       <DisplayFlag flag={props.oneCountry.flags.png} />
+  //       <h2>Weather in {props.oneCountry.capital[0]}</h2>
+  //       <p>temperature {props.weather.main.temp} Celcius</p>
+  //       <img src={`https://openweathermap.org/img/wn/${props.weather.weather[0].icon}@2x.png`}></img>
+  //       <p>wind {props.weather.wind.speed} m/s</p>
+  //     </div>
+  //   )
+  // }
 }
 
 
@@ -130,6 +146,8 @@ const App = () => {
 
   const [oneCountry, setOneCountry] = useState(null)
 
+  const [show, setShow] = useState(null)
+
   useEffect(() => {
     countryService.
       getAllCountryName()
@@ -138,34 +156,34 @@ const App = () => {
       })
   }, [])
 
-  //console.log(allCountries)
-
   // user input for country
   const handleCountry = (event) => {
-    //console.log(event.target.value)
     setUserCountry(event.target.value)
   }
 
   // show button
-
-  const toggleShow = (name, allCountries) => {
-    const filteredCountries = allCountries.filter(country => country.name.common === name)
+  const toggleShow = (name) => {
     countryService.getOne(name).then(oneCountry => {
       setOneCountry(oneCountry)
+      setShow(true)
     })
   }
 
-  // reset button
+  // reset the show when user change input
+  useEffect(() => {
+    setShow(null);
+  }, [userCountry])
 
-  const toggleReset = () => {
-    setOneCountry(null)
-    setUserCountry("")
+  let filteredCountries = allCountries.filter(country => country.name.common.toLowerCase().includes(userCountry.toLowerCase()))
+
+  if (show) {
+    filteredCountries = [oneCountry]
   }
 
   return (
     <div>
-      <CountryForm userCountry={userCountry} handleCountry={handleCountry} toggleReset={toggleReset} />
-      <Display userCountry={userCountry} allCountries={allCountries} toggleShow={toggleShow} oneCountry={oneCountry} setOneCountry={setOneCountry} />
+      <CountryForm userCountry={userCountry} handleCountry={handleCountry} />
+      <Display userCountry={userCountry} filteredCountries={filteredCountries} toggleShow={toggleShow} oneCountry={oneCountry} setOneCountry={setOneCountry} show={show} />
     </div>
   )
 
--- part2/countries/src/services/countries.jsx
+++ part2/countries/src/services/countries.jsx
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
 const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api/"
+const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&"
+const apiKey = import.meta.env.VITE_SOME_KEY
 
 const getAllCountryName = () => {
     const request = axios.get(`${baseUrl}all`)
-    //return request.then(response => response.data.map(country => country.name.common))
     return request.then(response => response.data)
 }
 
@@ -13,4 +14,9 @@ const getOne = (name) => {
     return request.then(response => response.data)
 }
 
-export default { getAllCountryName, getOne }
+const getWeather = (lat, lon) => {
+    const request = axios.get(`${weatherUrl}lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    return request.then(response => response.data)
+}
+
+export default { getAllCountryName, getOne, getWeather }
