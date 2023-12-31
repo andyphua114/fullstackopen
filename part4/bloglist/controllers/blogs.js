778ff62aa3aaf1a7a5e86f822b3bfc2d5479d3ab
wphua3 <wphua3@gatech.edu>
2023-12-31 12:30:21 +0800

    Complete until Ex 4.20

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,14 +1,22 @@
 const blogsRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog.js');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response, next) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response, next) => {
   const body = request.body;
 
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' });
+  }
+
+  const user = await User.findById(decodedToken.id);
   let likes = 0;
 
   if (Object.keys(body).includes('likes')) {
@@ -18,11 +26,17 @@ blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog({
     title: body.title,
     author: body.author,
+    user: user.id,
     url: body.url,
     likes: likes
   });
 
   const savedBlog = await blog.save();
+
+  // update the user notes array with the new created note id
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
   response.status(201).json(savedBlog);
 });
 
