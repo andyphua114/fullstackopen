6ec882d9f89543970c6c61c6aaffe003ff886bd7
wphua3 <wphua3@gatech.edu>
2024-01-01 15:47:02 +0800

    Complete until Ex 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,46 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  const [visible, setVisible] = useState(false);
+
+  const changeVisbility = () => {
+    setVisible(!visible);
+  };
+
+  if (!visible) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={changeVisbility}>view</button>
+      </div>
+    );
+  } else if (visible) {
+    let userName = null;
+    if (Object.keys(blog).includes('user')) {
+      userName = blog.user.name;
+    }
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author}
+          <button onClick={changeVisbility}>hide</button>
+        </div>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <div>{userName}</div>
+      </div>
+    );
+  }
+};
+
+export default Blog;
