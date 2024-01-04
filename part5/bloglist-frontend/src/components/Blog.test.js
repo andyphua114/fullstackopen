0f7f0c431f96ac871fd48a246d73751228124e84
wphua3 <wphua3@gatech.edu>
2024-01-04 21:00:55 +0800

    Complete until Ex 5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import Blog from './Blog'
 
+// Ex 5.13
 test('blog only shows title and author', () => {
   const initialBlogs = [
     {
@@ -22,4 +23,38 @@ test('blog only shows title and author', () => {
 
   const element = screen.getByText('This is a blog title Author A')
   expect(element).toBeDefined()
+  const url = screen.queryByText('https://thisisablogtitle.com')
+  expect(url).toBeNull()
+})
+
+// Ex 5.14
+test('blog shows title and author, url, likes when clicked on view', async () => {
+  const initialBlogs = [
+    {
+      title: 'This is a blog title',
+      author: 'Author A',
+      url: 'https://thisisablogtitle.com'
+    },
+    {
+      title: 'Adding a second blog',
+      author: 'Writer B',
+      url: 'https://addingasecondblog.com'
+    }
+  ]
+
+  const container = render(<Blog blog={initialBlogs[0]} />).container
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const div = container.querySelector('.blog')
+  const element = screen.getByText('This is a blog title Author A')
+  expect(element).toBeDefined()
+
+  const url = screen.getByText('https://thisisablogtitle.com')
+  expect(url).toBeDefined()
+
+  const likes = await screen.findByText('likes')
+  expect(likes).toBeDefined()
 })
