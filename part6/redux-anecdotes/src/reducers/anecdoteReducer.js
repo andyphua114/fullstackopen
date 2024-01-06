15e97738e2a80f50ecc2371dfa11149078a43d24
wphua3 <wphua3@gatech.edu>
2024-01-06 18:10:38 +0800

    Complete until Ex 6.4

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,6 +23,8 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
   switch (action.type) {
+    case 'NEW':
+      return [...state, action.payload]
     case 'VOTE':
       const id = action.payload.id
       const anecdoteToChange = state.find((n) => n.id === id)
@@ -30,6 +32,13 @@ const reducer = (state = initialState, action) => {
       return state.map((anecdote) => (anecdote.id !== id ? anecdote : changedAnecdote))
     default:
       return state
+  }
+}
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW',
+    payload: asObject(content)
   }
 }
 
