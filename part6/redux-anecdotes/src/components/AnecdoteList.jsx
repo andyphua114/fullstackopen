bc5e9951a72458ec8beb7300922ce859c57fd88c
wphua3 <wphua3@gatech.edu>
2024-01-07 10:36:29 +0800

    Complete until Ex 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -2,7 +2,15 @@ import { useDispatch, useSelector } from 'react-redux'
 import { increaseVote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state)
+  const anecdotes = useSelector((state) => {
+    if (state.filter === '') {
+      return state.anecdotes
+    } else {
+      return state.anecdotes.filter((anecdote) =>
+        anecdote.content.toLowerCase().includes(state.filter.toLowerCase())
+      )
+    }
+  })
   const dispatch = useDispatch()
 
   const vote = (id) => {
